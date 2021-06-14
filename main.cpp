42c8767a00c9152cf0961818e88fbae4e39c6794
OldScripter <Oldscripter@gmail.com>
2021-06-14 03:10:56 +0300

    Working version - need refactoring

--- main.cpp
+++ main.cpp
@@ -1,70 +1,47 @@
 #include <iostream>
-#include <cassert>
 #include <sstream>
 
-double GetDoubleFromSubString(int start_index, int end_index, std::string str)
+int main()
 {
-    assert(start_index >= 0 && start_index < str.length());
-    assert(end_index >= 0 && end_index < str.length());
+    std::string buffer;
+    std::cout << "Please enter the expression <Num1><Operation><Num2>:\n";
+    std::cin >> buffer;
 
-    std::cout << "Your string on input: " << str << "\n";
-    std::string sub_string;
-
-    for (int i = start_index; i < end_index; i++)
-    {
-        sub_string += str[i];
-    }
-    return std::stod(sub_string);
-}
-
-int main() {
-
-
-
-    std::string expression;
-
-    std::cout << "Please enter the expresion in format <first_num><operation><second_num>: ";
-    std::cin >> expression;
-
-    double first_number = 0;
-    double second_number = 0;
     char operation = 0;
+    double num_1 = 0;
+    double num_2 = 0;
 
-    for (int i = 0; i < expression.size(); i++)
+    std::string num_1_str;
+    std::string num_2_str;
+
+    for (int i = 0; i < buffer.length(); i++)
     {
-        if (expression[i] == '+' ||
-            expression[i] == '-' ||
-            expression[i] == '*' ||
-            expression[i] == '/')
+
+        if (buffer[i] == '+' || buffer[i] == '-' || buffer[i] == '*' || buffer[i] == '/')
         {
-            first_number = GetDoubleFromSubString(0, i, expression);
-            second_number = GetDoubleFromSubString(i + 1, expression.size(), expression);
-            operation = expression[i];
-
-            std::cout << "NUM_1: " << first_number <<'\n';
-            std::cout << "NUM_2: " << second_number <<'\n';
-            std::cout << "Oper: " << operation <<'\n';
-
-            break;
+            operation = buffer[i];
+        }
+        else if (operation == 0)
+        {
+            num_1_str += buffer[i];
+        }
+        else
+        {
+            num_2_str += buffer[i];
         }
     }
-    double res;
 
-    if (operation == '+') res = first_number + second_number;
-    else if (operation == '-') res = first_number - second_number;
-    else if (operation == '*') res = first_number * second_number;
-    else if (operation == '/')
-    {
-        if (second_number == 0)
-        {
-            std::cout << "Division on zero!\n";
-            return 1;
-        }
-        else res = first_number / second_number;
-    }
+    num_1 = std::stod(num_1_str);
+    num_2 = std::stod(num_2_str);
 
-    std::cout << first_number << operation << second_number << " = " << "\n";
-    std::printf("%.f \n" , res);
+    double result = 0;
+    if (operation == '+') result = num_1 + num_2;
+    else if (operation == '-') result = num_1 - num_2;
+    else if (operation == '*') result = num_1 * num_2;
+    else if (operation == '/' && num_2 != 0) result = num_1 / num_2;
+    else std::cout << "Error!\n";
 
+    printf("%f", result);
+    
     return 0;
 }
